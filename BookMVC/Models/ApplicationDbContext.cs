ed44a81c42d7d9d3c8ee9985ddc37df079aa13ae
Matthew Ajayi <ajayi.m@outlook.com>
2023-03-16 23:31:24 -0400

    Add project files.

---
+++ BookMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set;}
+    }
+}
